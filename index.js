e98602a27f273c85edac856024cb53c67e66b4e9
Petri Kataja <peejkoo@gmail.com>
2019-12-08 14:21:47 +0200

    3.2 added /info

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+  let now = new Date()
+  let html = `<p>Phonebook has info for ${persons.length} people</p>`
+  html += `<p>${now}</p>`
+  res.send(html)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
