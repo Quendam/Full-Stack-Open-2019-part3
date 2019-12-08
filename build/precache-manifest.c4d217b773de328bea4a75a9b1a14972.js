e01b5c63409ff8eceb9d4cdf54546383775ae093
Petri Kataja <peejkoo@gmail.com>
2019-12-08 20:26:08 +0200

    uibuild

---
+++ build/precache-manifest.c4d217b773de328bea4a75a9b1a14972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2da15bcb4a9567c467821dacafb415",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbc97ed9d75c853c22f9",
+    "url": "/static/css/main.0c14b618.chunk.css"
+  },
+  {
+    "revision": "65dedc7de98df58488da",
+    "url": "/static/js/2.53b0bc39.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.53b0bc39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbc97ed9d75c853c22f9",
+    "url": "/static/js/main.4928a2bc.chunk.js"
+  },
+  {
+    "revision": "3305686cfe022fad0350",
+    "url": "/static/js/runtime-main.4ce057ac.js"
+  }
+]);
